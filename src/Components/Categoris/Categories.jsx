669066f6a0ca482c63d3b95ad1c2fb5aa33f7a01
Rihanul Islam <rihanthedev@gmail.com>
2023-07-17 23:40:48 +0600

    Categories

---
+++ src/Components/Categoris/Categories.jsx
@@ -0,0 +1,15 @@
+
+const Categories = () => {
+    return (
+        <div>
+            <div className='divider w-3/1 my-4'>
+                <p className='text-4xl font-serif text-orange-400'>Browse by Categories</p>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default Categories;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import FlaseSale from '../../Components/FlaseSale/FlaseSale';
+import Categories from '../../Components/Categoris/Categories';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <FlaseSale></FlaseSale>
+            <Categories></Categories>
         </div>
     );
 };
