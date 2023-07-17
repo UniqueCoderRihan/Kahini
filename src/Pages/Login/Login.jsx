9bfaba86808ac1e5801ff898fefeb9b1ba3cb5a1
Rihanul Islam <rihanthedev@gmail.com>
2023-07-17 15:24:31 +0600

    Login Added

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
-
+import { FaGoogle,FaFacebookSquare} from "react-icons/fa";
+import { Link } from "react-router-dom";
 const Login = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
@@ -13,7 +14,7 @@ const Login = () => {
         //     </select>
         //     <input type="submit" />
         // </form>
-        <div className="bg-base-200 mx-auto sm:w-1/3 p-5 ">
+        <div className="bg-base-200 mx-auto sm:w-1/3 p-5 my-4 rounded-3xl">
             <img src="https://i.ibb.co/M2GJH3g/techzoxne-1.png" alt="techzoxne-1" className='rounded-lg p-5' />
             <p className="text-3xl ">Please Login Your Account</p>
             <form>
@@ -28,12 +29,25 @@ const Login = () => {
                         <label className="label">
                             <span className="label-text">Password</span>
                         </label>
-                        <input type="text" placeholder="email" className="input input-bordered" />
+                        <input type="text" placeholder="PassWord" className="input input-bordered" />
+                    </div>
+                    <div className="flex">
+                        <p><small>Forget Password?</small></p>
+                        <p className="flex justify-end"><small><Link to='/register'>Are you new?</Link></small></p>
                     </div>
                     <button className="btn btn-secondary">Login</button>
                 </div>
                 
             </form>
+            <div className="divider mt-0"></div>
+            <div className="text-center">
+                <button className="btn btn-outline btn-circle text-2xl">
+                    <FaGoogle></FaGoogle>
+                </button>
+                <button className="btn btn-outline btn-circle text-2xl ml-2">
+                    <FaFacebookSquare></FaFacebookSquare>
+                </button>
+            </div>
         </div>
     );
 };
