8e15fdd0b2a22294cbf1bfdad47b480e8c824888
uniquecoderrihan <rihanthedev@gmail.com>
2023-09-27 00:51:40 +0600

    Categories Selected

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,14 @@ import Banner from '../../Components/Banner/Banner';
 import FlaseSale from '../../Components/FlaseSale/FlaseSale';
 import Categories from '../../Components/Categoris/Categories';
 import Info from '../../Components/info/Info';
+import Categoris from './categoris/Categoris';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Info/>
-            {/* <FlaseSale></FlaseSale> */}
-            <Categories></Categories>
+            <Categoris/>
         </div>
     );
 };
