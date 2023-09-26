1466e8bf33dfa483944cdbb664632743c76dcd74
uniquecoderrihan <rihanthedev@gmail.com>
2023-09-26 23:56:39 +0600

    Service Terms Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import FlaseSale from '../../Components/FlaseSale/FlaseSale';
 import Categories from '../../Components/Categoris/Categories';
+import Info from '../../Components/info/Info';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <FlaseSale></FlaseSale>
+            <Info/>
+            {/* <FlaseSale></FlaseSale> */}
             <Categories></Categories>
         </div>
     );
