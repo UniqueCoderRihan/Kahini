1f977bd273d1525018b4f2b0b19c76aa2770fe1e
uniquecoderrihan <rihanthedev@gmail.com>
2023-09-27 00:22:56 +0600

    Navbar Updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,57 +3,119 @@ import { FaCartPlus } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 import { AuthContex } from '../../Provider/AuthProvider';
 const Navbar = () => {
-    const { user,logout } = useContext(AuthContex);
+    const { user, logout } = useContext(AuthContex);
     console.log(user);
-    const handleLogout = ()=>{
+    const handleLogout = () => {
         logout()
     }
     const items = <>
-        <li className='text-2xl px-2'> <Link to='/'>Home</Link> </li>
-        <li className='text-2xl px-2'> <Link to='/all-products'>All Products</Link> </li>
-        <li className='text-2xl px-2'> <Link to='/blog'>Blog</Link> </li>
+        <li>
+            <Link to='/'>Home</Link>
+        </li>
+        <li>
+            <Link to='/shop'>Shop</Link>
+        </li>
+        <li>
+            <Link to='/collection'>Collection</Link>
+        </li>
+        <li>
+            <Link to='/blogs'>Blog</Link>
+        </li>
 
         {
             user ? <>
-                <li className='text-2xl px-2'> <Link to='/dashbord'>Dashbord</Link> </li>
+                <li className=' px-2'> <Link to='/dashbord'>Dashbord</Link> </li>
                 <div className="avatar">
                     <div className="w-12  rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
                         <img src={user.photoURL} />
                     </div>
                 </div>
-                <li className='text-2xl px-2'> <Link onClick={handleLogout}>Logout</Link> </li>
+                <li className=' px-2'> <Link onClick={handleLogout}>Logout</Link> </li>
             </> : <>
-                <li className='text-2xl px-2'> <Link to='/login'>Login</Link> </li>
+                <li className=' px-2'> <Link to='/login'>Login</Link> </li>
             </>
         }
     </>
     return (
-        <div className="navbar bg-base-300 py-3 ">
-            <div className="navbar-start ml-5">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-                        {items}
-
-                    </ul>
+        <header className="border-b border-black">
+            <div className="navbar">
+                <div className="navbar-start">
+                    <div className="dropdown md:hidden">
+                        <label tabIndex={0} className="btn btn-ghost btn-circle">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h7"
+                                />
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                        >
+                            {/* responsive Menubar */}
+                            {items}
+                        </ul>
+                    </div>
+                    <div className="hidden md:block">
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-horizontal"
+                        >
+                            {items}
+                        </ul>
+                    </div>
                 </div>
-                <img src="https://i.ibb.co/M2GJH3g/techzoxne-1.png" alt="techzoxne-1" className='rounded-lg' />
+                <div className="navbar-center">
+                    <Link to={'/'} className="font-bold text-xl lg:text-2xl">Kahini</Link>
+                </div>
+                <div className="navbar-end">
+                    <button className="btn btn-ghost btn-circle">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                            />
+                        </svg>
+                    </button>
+                    <button className="btn btn-ghost btn-circle">
+                        <div className="indicator">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                                />
+                            </svg>
+                            <span className="badge badge-xs badge-primary indicator-item"></span>
+                        </div>
+                    </button>
+                </div>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    {items}
-                </ul>
-            </div>
-            <div className="navbar-end mr-5">
-                <button className="btn">
-                    <FaCartPlus className='text-4xl'></FaCartPlus>
-                    <div className="badge badge-secondary">0</div>
-                </button>
-
-            </div>
-        </div>
+        </header>
     );
 };
 
