484c434c4d73bc6b7c7076f94bbe678de6b06c39
uniquecoderrihan <rihanthedev@gmail.com>
2023-09-28 17:26:06 +0600

    data simple

---
+++ public/simple.js
@@ -0,0 +1,21 @@
+/**
+*
+*[
+  {
+    product_name: "Appple Pro Max",
+    product_img: "https://www.digitaltrends.com/wp-content/uploads/2021/11/macbook-pro-2021-01.jpg?p=1",
+    product_description: "This is World Class Most Handsome Product. This Available on the Whole lorem ispat This Available on the Whole lorem ispat This Available on the Whole lorem ispat This Available on the Whole lorem ispat This Available on the Whole lorem ispat This Available on the Whole lorem ispat This Available on the Whole lorem ispat",
+    product_price: 4753,
+    available_in_stock: 70,
+    product_seller: "Ibrahim Khan",
+    seller_email: 'seller1@gmail.com',
+    seller_img: 'https://camo.githubusercontent.com/62da68eb62b1eML4xyCZyjRudsz4QfPmhK2n2bdAxCCd37d4a0ab07365d6791/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4d3967624264396e6244724f5475314d71782f67697068792e676966',
+    ratings: [
+      { buyer_name: "Raihan alom", rate: 4, feedback: 'Good Product. You can buy It' },
+      { buyer_name: "Ibrahim alom", rate: 1, feedback: 'Bad Product. You never buy It' }
+    ]
+  }
+];
+*
+*
+*/ 
